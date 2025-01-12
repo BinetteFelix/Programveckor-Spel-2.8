310d5adb2a02bb1519d7a0c43af7e2d4d50b0505
TedRosling <ted.rosling@elev.ga.lbs.se>
2025-01-12 04:31:22 +0100

    Accurate night sky at 65.6 , 18.5

--- Assets/Scripts/Enviroment/DayAndNightCycle.cs
+++ Assets/Scripts/Enviroment/DayAndNightCycle.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
@@ -8,19 +9,39 @@ public class DayAndNightCycle : MonoBehaviour
 
     public string currentTimeString;
     public Light sunLight;
-    public float sunPosition = 1f;
+    //Luleå har en lattitud på ungefär 65.6 och ca 22.2 longitude
+    //spelet utspelar sig där nån stans fast typ i skogen så vi tar bort lite
+    public float sunLatitude = 65.6f;
+    public float sunLongitude = 18.5f;
     public float sunIntensity = 1f;
     public AnimationCurve sunIntensityMultiplier;
-    public AnimationCurve lightTempratureCurve;
+    public AnimationCurve sunTempratureCurve;
+
+    public float moonLatitude = 65.6f;
+    public float moonLongitude = 18.5f;
+    public Light moonLight;
+    public float moonIntensity = 1f;
+    public AnimationCurve moonIntensityMultiplier;
+    public AnimationCurve moonTempratureCurve;
+
+
+    public VolumeProfile volumeProfile;
+    private PhysicallyBasedSky skySettings;
+    public float starIntensity = 1f;
+    public AnimationCurve starCurve;
+    public float polarstarLatitude = 65.6f;
+    public float polarstarLongitude = 18.5f;
 
     public bool isDay = true;
 
-    public Light moonLight;
+    public bool sunActive = true;
+    public bool moonActive = true;
 
     private void Start()
     {
         UpdateTimeText();
         CheckShadowStatus();
+        SkyStars();
     }
 
     private void Update()
@@ -35,12 +56,14 @@ public class DayAndNightCycle : MonoBehaviour
         UpdateTimeText();
         UpdateLight();
         CheckShadowStatus();
+        SkyStars();
     }
 
     private void OnValidate()
     {
         UpdateLight();
         CheckShadowStatus();
+        SkyStars();
     }
 
     void UpdateTimeText()
@@ -51,26 +74,42 @@ public class DayAndNightCycle : MonoBehaviour
     void UpdateLight()
     {
         float sunRotation = currentTime / 24f * 360f;
-        sunLight.transform.rotation = Quaternion.Euler(sunRotation - 90, sunPosition, 0f);
-        moonLight.transform.rotation = Quaternion.Euler(sunRotation + 90, sunPosition, 0f);
+        sunLight.transform.localRotation = (Quaternion.Euler(sunLatitude - 90, sunLongitude, 0) * Quaternion.Euler(0, sunRotation, 0));
+        moonLight.transform.localRotation = (Quaternion.Euler(90 - moonLatitude, moonLongitude, 0) * Quaternion.Euler(0, sunRotation, 0));
 
         float normalizedTime = currentTime / 24f;
-        float intensityCurve = sunIntensityMultiplier.Evaluate(normalizedTime);
+        float sunIntensityCurve = sunIntensityMultiplier.Evaluate(normalizedTime);
+        float moonIntensityCurve = moonIntensityMultiplier.Evaluate(normalizedTime);
 
         HDAdditionalLightData sunLightData = sunLight.GetComponent<HDAdditionalLightData>();
+        HDAdditionalLightData moonLightData = moonLight.GetComponent<HDAdditionalLightData>();
+
 
         if (sunLightData != null)
         {
-            float i = intensityCurve * sunIntensity;
+            float i = sunIntensityCurve * sunIntensity;
             sunLightData.intensity = i;
         }
-
-        float tempratureMultiplier = lightTempratureCurve.Evaluate(normalizedTime);
-        Light lightComponent = sunLight.GetComponent<Light>();
-
-        if (lightComponent != null)
+        if (moonLightData != null)
         {
-            lightComponent.colorTemperature = tempratureMultiplier * 10000f;
+            float i = moonIntensityCurve * moonIntensity;
+            moonLightData.intensity = i;
+        }
+
+        float sunTempratureMultiplier = sunTempratureCurve.Evaluate(normalizedTime);
+        float moonTempratureMultiplier = moonTempratureCurve.Evaluate(normalizedTime);
+
+
+        Light sunLightComponent = sunLight.GetComponent<Light>();
+        Light moonLightComponent = moonLight.GetComponent<Light>();
+
+        if (sunLightComponent != null)
+        {
+            sunLightComponent.colorTemperature = sunTempratureMultiplier * 10000f;
+        }
+        if (moonLightComponent != null)
+        {
+            moonLightComponent.colorTemperature = moonTempratureMultiplier * 10000f;
         }
     }
 
@@ -93,6 +132,36 @@ public class DayAndNightCycle : MonoBehaviour
 
             isDay = false;
         }
+
+        if (currentTime >= 5.7f && currentTime <= 18.3f)
+        {
+            sunLight.gameObject.SetActive(true);
+            sunActive = false;
+        }
+        else
+        {
+            sunLight.gameObject.SetActive(false);
+            sunActive = true;
+        }
+
+        if (currentTime >= 6.3f && currentTime <= 17.7f)
+        {
+            moonLight.gameObject.SetActive(false);
+            moonActive = true;
+        }
+        else
+        {
+            moonLight.gameObject.SetActive(true);
+            moonActive = false;
+        }
+    }
+
+    void SkyStars()
+    {
+        volumeProfile.TryGet<PhysicallyBasedSky>(out skySettings);
+        skySettings.spaceEmissionMultiplier.value = starCurve.Evaluate(currentTime / 24f) * starIntensity;
+
+        skySettings.spaceRotation.value = (Quaternion.Euler(90 - polarstarLatitude, polarstarLongitude, 0) * Quaternion.Euler(0, currentTime / 24 * 360, 0)).eulerAngles;
     }
 
 }
