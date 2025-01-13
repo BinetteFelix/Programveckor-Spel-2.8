b1fa7a105363a30f531e627fefa57779e89880d1
BinetteFelix <felix.binette@elev.ga.lbs.se>
2025-01-13 16:38:03 +0100

    Player ADS in i main. inventory i main fiunkar helt
    fortfarande en liten glitch med att popupen när man plockar upp nåt kommer upp på fel ställe men nu är det ingen error iaf

--- Assets/Scripts/Inventory/POPUP.cs
+++ Assets/Scripts/Inventory/POPUP.cs
@@ -1,22 +1,21 @@
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class POPUP : MonoBehaviour
 {
-    public TextMeshProUGUI PopUpText; // Pop-up text prefab
+    [SerializeField] private TextMeshProUGUI PopUpText; // Pop-up text prefab
     private TextMeshProUGUI popuptext; // Private reference object
 
-    Vector3 SpawnOffset = new Vector3(5, 5, 0); // Spawn offset for pop-up text
+    private Vector3 SpawnOffset = new Vector3(0, 20, 0); // Spawn offset for pop-up text
 
-    bool _IsActive = false; // bool for pop-up state
+    private bool _IsActive = false; // bool for pop-up state
 
     // Update is called once per frame
     void Update()
     {
         if (_IsActive) // If the pop-up is active
         {
-            popuptext.transform.position = Camera.main.WorldToScreenPoint(transform.position) + SpawnOffset; // set pop-up's position to the item object + an offset
+            popuptext.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position) + SpawnOffset; // set pop-up's position to the item object + an offset
         }
     }
     public void OpenPopUp()
@@ -26,7 +25,6 @@ public class POPUP : MonoBehaviour
             _IsActive = true; // Say that the pop-up is active
             popuptext = Instantiate(PopUpText, FindAnyObjectByType<Canvas>().transform).GetComponent<TextMeshProUGUI>(); // Instantiate pop-up prefab
         }
-        
     }
     public void ClosePopUp()
     {
